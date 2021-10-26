672fb6335c98649edf41d627927288813c674594
chin <a5566qq2581@gmail.com>
2021-10-26 20:18:44 +0800

    fix: resolve the problem that time of primitive type can't store in schema

--- models/mongodb/FHIRDataTypesSchema/time.js
+++ models/mongodb/FHIRDataTypesSchema/time.js
@@ -1,8 +1,23 @@
 const moment = require('moment');
 module.exports = {
-    type: Date,
+    type: Number,
     default: void 0,
+    set: function(v) {
+        let timeDate = moment(v, "hh:mm:ss.SSS").toDate();
+        let storeTime = timeDate.getHours() * 3600000 + timeDate.getMinutes() * 60000+ timeDate.getSeconds() * 1000 + timeDate.getMilliseconds();
+        console.log(storeTime);
+        return storeTime;
+    },
     get: function(v) {
-        return moment(v).format('hh:mm:ss');
+        let hours = parseInt(v / 3600000);
+        let leaves = v - hours * 3600000;
+        let minutes = parseInt(leaves / 60000);
+        let leavesSeconds = (leaves - minutes * 60000);
+        let seconds = parseInt(leavesSeconds / 1000);
+        let milliSeconde = leavesSeconds - seconds * 1000;
+        if (milliSeconde > 0 ) {
+            return `${hours.toString().padStart(2,"00")}:${minutes.toString().padStart(2,"00")}:${seconds.toString().padStart(2,"00")}.${milliSeconde.toString().padStart(3,"000")}`;
+        }
+        return `${hours.toString().padStart(2,"00")}:${minutes.toString().padStart(2,"00")}:${seconds.toString().padStart(2,"00")}`;
     }
 }
