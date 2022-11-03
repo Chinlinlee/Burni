5133ae1cecfc0d698c1d96865893ee463d498e99
chin <a5566qq2581@gmail.com>
2022-11-04 00:00:16 +0800

    feat: move FHIR XML to JSON into `route.js`
    - Use before plugin loading,
    for converting XML FHIR content to JSON and use in the plugin
    - Remove function of converting XML to JSON
    in each resourceType's entry route
    > avoid convert again

--- api_generator/API_Generator_V2.js
+++ api_generator/API_Generator_V2.js
@@ -241,46 +241,8 @@ function generateAPI(option) {
         const {
             FHIRValidateParams
         } = require('api/validator');
-        const FHIR = require('fhir').Fhir;
-        const { handleError } = require('../../../models/FHIR/httpMessage');
         const _ = require('lodash');
         const config = require('../../../config/config');
-
-        function setFormatWhenQuery (req , res) {
-            let format = _.get(req , "query._format");
-            if (format && format.includes("xml")) {
-                res.set('Content-Type', 'application/fhir+xml');
-            } else if (format && format.includes("json")) {
-                res.set('Content-Type', 'application/fhir+json');
-            }
-            delete req['query']['_format'];
-        }
-
-        router.use((req, res, next) => {
-            try {
-                if (req.headers["content-type"]) {
-                    if (req.headers["content-type"].includes("xml")) {
-                        res.set('Content-Type', 'application/fhir+xml');
-                        if (req.method == "POST" || req.method == "PUT") {
-                            let Fhir = new FHIR();
-                            req.body = Fhir.xmlToObj(req.body);
-                        }
-                    }
-                }
-                _.get(req.headers, "accept") ? "" : ( ()=> {
-                    _.get(req.headers , "content-type") ? _.set(req.headers , "accept" , _.get(req.headers , "content-type")) : _.set(req.headers , "accept" , "application/fhir+json");
-                })();
-                if (req.headers.accept.includes("xml")) {
-                    res.set('Content-Type', 'application/fhir+xml');
-                } else {
-                    res.set('Content-Type', 'application/fhir+json');
-                }
-                setFormatWhenQuery(req , res);
-                next();
-            } catch (e) {
-                return res.send(handleError.exception(e));
-            }
-        });
 
         if (_.get(config, "${res}.interaction.search", true)) {
             router.get('/', FHIRValidateParams({
--- routes.js
+++ routes.js
@@ -1,7 +1,56 @@
 const fs = require('fs');
+const _ = require("lodash");
+const {
+    handleError
+} = require('./models/FHIR/httpMessage');
+const FHIR = require('fhir').Fhir;
 const { pluginsConfig } = require("./plugins/config");
 
+function setFormatWhenQuery(req, res) {
+    let format = _.get(req, "query._format");
+    if (format && format.includes("xml")) {
+        res.set('Content-Type', 'application/fhir+xml');
+    } else if (format && format.includes("json")) {
+        res.set('Content-Type', 'application/fhir+json');
+    }
+    delete req['query']['_format'];
+}
+
 module.exports = function (app) {
+
+    //#region fhir
+    let fhirDir = fs.readdirSync("./api/FHIR");
+    for (let dir of fhirDir) {
+        let isDir = fs.lstatSync(`./api/FHIR/${dir}`).isDirectory();
+        if (isDir) {
+            app.use(`/${process.env.FHIRSERVER_APIPATH}/${dir}`, (req, res, next) => {
+                try {
+                    if (req.headers["content-type"]) {
+                        if (req.headers["content-type"].includes("xml")) {
+                            res.set('Content-Type', 'application/fhir+xml');
+                            if (req.method == "POST" || req.method == "PUT") {
+                                let Fhir = new FHIR();
+                                req.body = Fhir.xmlToObj(req.body);
+                            }
+                        }
+                    }
+                    _.get(req.headers, "accept") ? "" : (() => {
+                        _.get(req.headers, "content-type") ? _.set(req.headers, "accept", _.get(req.headers, "content-type")) : _.set(req.headers, "accept", "application/fhir+json");
+                    })();
+                    if (req.headers.accept.includes("xml")) {
+                        res.set('Content-Type', 'application/fhir+xml');
+                    } else {
+                        res.set('Content-Type', 'application/fhir+json');
+                    }
+                    setFormatWhenQuery(req, res);
+                    next();
+                } catch (e) {
+                    return res.send(handleError.exception(e));
+                }
+            });
+        }
+    }
+    //#endregion
 
     for(let pluginName in pluginsConfig) {
         let plugin = pluginsConfig[pluginName];
@@ -13,7 +62,6 @@ module.exports = function (app) {
     
 
     //#region fhir
-    let fhirDir = fs.readdirSync("./api/FHIR");
     for (let dir of fhirDir) {
         let isDir = fs.lstatSync(`./api/FHIR/${dir}`).isDirectory();
         if (isDir) {
