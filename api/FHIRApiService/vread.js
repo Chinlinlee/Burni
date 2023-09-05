736e09a6b683c8287e41ef6ef38166b0ee4452f9
chin <a5566qq2581@gmail.com>
2023-09-05 21:21:49 +0800

    refactor: extract vread API to service class

--- api/FHIRApiService/vread.js
+++ api/FHIRApiService/vread.js
@@ -1,8 +1,9 @@
 const mongodb = require("models/mongodb");
 const { handleError } = require("../../models/FHIR/httpMessage");
 const FHIR = require("fhir").Fhir;
-const { logger } = require("../../utils/log");
+const { logger } = require("@root/utils/log");
 const path = require("path");
+const { VReadService } = require("./services/vread.service");
 
 /**
  * @param {import("express").Request} req
@@ -16,46 +17,13 @@ module.exports = async function (req, res, resourceType) {
             "content-type"
         )}]`
     );
-    let doRes = function (code, item) {
-        if (res.getHeader("content-type").includes("xml")) {
-            let fhir = new FHIR();
-            let xmlItem = fhir.objToXml(item);
-            return res.status(code).send(xmlItem);
-        }
-        return res.status(code).send(item);
-    };
-    let id = req.params.id;
-    let version = req.params.version;
-    try {
-        let docs = await mongodb[`${resourceType}_history`]
-            .findOne({
-                $and: [
-                    {
-                        id: id
-                    },
-                    {
-                        "meta.versionId": version
-                    }
-                ]
-            })
-            .exec();
-        if (docs) {
-            let responseDoc = docs.getFHIRField();
-            res.header(
-                "Last-Modified",
-                new Date(responseDoc.meta.lastUpdated).toUTCString()
-            );
-            return doRes(200, responseDoc);
-        }
-        let errorMessage = `not found ${resourceType}/${id} with version ${version} in history`;
-        let operationOutcomeError = handleError.exception(errorMessage);
-        return doRes(404, operationOutcomeError);
-    } catch (e) {
-        console.log(
-            `api ${process.env.FHIRSERVER_APIPATH}/${resourceType}/:id has error, `,
-            e
-        );
-        let operationOutcomeError = handleError.exception(e);
-        return doRes(500, operationOutcomeError);
+
+    let vReadService = new VReadService(req, res, resourceType);
+    let { status, code, result } = await vReadService.versionRead();
+    if (!status) {
+        logger.error(`API ${process.env.FHIRSERVER_APIPATH}/${resourceType}/:id error occurred, ${JSON.stringify(result, Object.getOwnPropertyNames(result))}`);
+        return vReadService.doResponse(code, result);
     }
+
+    return vReadService.doSuccessResponse(result);
 };
