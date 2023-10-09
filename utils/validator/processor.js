b484e7df90df110a7b5c335c7bede657023d1f47
chinlinlee <a5566qq2581@gmail.com>
2023-10-09 19:32:26 +0800

    refactor: use es2022 refactoring validator processor

--- utils/validator/processor.js
+++ utils/validator/processor.js
@@ -5,41 +5,24 @@ const { validator } = require("./index.js");
  * @param {Object} resource
  */
 async function validateResource(resource) {
-    let operationOutcome;
+    let operationOutcome = await validator.validateFromBuffer(
+        Buffer.from(JSON.stringify(resource)),
+        resource.meta?.profile?.join(",")
+    );
 
-    let meta = Object.prototype.hasOwnProperty.call(resource, "meta")
-        ? resource.meta
-        : undefined;
-    if (meta) {
-        let profile = Object.prototype.hasOwnProperty.call(meta, "profile")
-            ? meta.profile.join(",")
-            : undefined;
-        operationOutcome = await validator.validateFromBuffer(
-            Buffer.from(JSON.stringify(resource)),
-            profile
-        );
-    } else {
-        operationOutcome = await validator.validateFromBuffer(
-            Buffer.from(JSON.stringify(resource)),
-            undefined
-        );
-    }
-    
     if (Object.prototype.hasOwnProperty.call(operationOutcome, "issue")) {
-        let isError = operationOutcome.issue.some(
-            (v) => v.severity === "error"
-        );
-        if (isError) {
-            return {
-                isError: true,
-                message: operationOutcome
-            };
-        }
         return {
-            isError: false,
+            isError: operationOutcome.issue.some(
+                (v) => v.severity === "error" || v.severity === "fatal"
+            ),
             message: operationOutcome
         };
     }
+
+    return {
+        isError: false,
+        message: operationOutcome
+    };
 }
 
 module.exports.validateResource = validateResource;
