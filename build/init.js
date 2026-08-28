ef859ec2094b1e7efd9891ee346b3071a3003b81
chinlinlee <a5566qq2581@gmail.com>
2026-08-29 00:41:09 +0800

    chore(build): document validator env vars in init template
    - Add VALIDATOR_URL and VALIDATOR_TIMEOUT_MS comments to generated .env

--- build/init.js
+++ build/init.js
@@ -35,6 +35,10 @@ ADMIN_PASSWORD="password"
 ENABLE_CHECK_ALL_RESOURCE_ID=false
 
 ENABLE_VALIDATOR=true
+# Required when ENABLE_VALIDATOR=true. Full Inferno POST /validate endpoint.
+# VALIDATOR_URL=http://localhost:4567/validate
+# Optional. Positive integer milliseconds. Defaults to 30000.
+# VALIDATOR_TIMEOUT_MS=30000
 `;
         fs.writeFileSync(".env", envText);
         console.log(
